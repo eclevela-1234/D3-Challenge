17a0a31fdd511c66b3c89effe611d56c355838fc
Cleveland <eclevela@richmond.edu>
2021-07-22 17:21:58 -0400

    adding csv load

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,33 @@
 // @TODO: YOUR CODE HERE!
+
+// Set SVG size and margins
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 60,
+  left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+d3.csv("../assets/data/data.csv").then(function(healthData) {
+
+  console.log(healthData);
+
+//   // log a list of names
+//   var names = tvData.map(data => data.name);
+//   console.log("names", names);
+
+//   // Cast each hours value in tvData as a number using the unary + operator
+//   tvData.forEach(function(data) {
+//     data.hours = +data.hours;
+//     console.log("Name:", data.name);
+//     console.log("Hours:", data.hours);
+//   });
+// }).catch(function(error) {
+//   console.log(error);
+});

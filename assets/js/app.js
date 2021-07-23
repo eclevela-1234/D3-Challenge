e5fa8bc2fafd1f27eb7ba6d0c21554b2c2921218
Cleveland <eclevela@richmond.edu>
2021-07-23 08:26:31 -0400

    adding file path comment

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,20 +14,9 @@ var margin = {
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
-d3.csv("../assets/data/data.csv").then(function(healthData) {
+// this is the file path for the data!
+// d3.csv("../assets/data/data.csv").then(function(healthData) {
 
-  console.log(healthData);
+//   console.log(healthData);
 
-//   // log a list of names
-//   var names = tvData.map(data => data.name);
-//   console.log("names", names);
 
-//   // Cast each hours value in tvData as a number using the unary + operator
-//   tvData.forEach(function(data) {
-//     data.hours = +data.hours;
-//     console.log("Name:", data.name);
-//     console.log("Hours:", data.hours);
-//   });
-// }).catch(function(error) {
-//   console.log(error);
-});
